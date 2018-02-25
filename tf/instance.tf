9bd9ce303052e21c5b2bfc6df208c37b089fd487
Mathew Thomas <mathew.thomas@longdivision.com>
2018-02-25 18:21:09 -0500

    Ubuntu 17 AMI
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- tf/instance.tf
+++ tf/instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bc-redis-1" {
-  ami           = "ami-2757f631"
+  ami           = "ami-5cd4a126"
   instance_type = "t2.micro"
   subnet_id = "subnet-9eb33ea1"
   vpc_security_group_ids = ["sg-24264f51"]
