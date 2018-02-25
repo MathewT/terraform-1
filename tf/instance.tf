c306d8210566798e05170c6ed683bc834b992135
Mathew Thomas <mathew.thomas@longdivision.com>
2018-02-25 18:12:07 -0500

    vpc subnet and sg
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- tf/instance.tf
+++ tf/instance.tf
@@ -1,4 +1,6 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "bc-redis-1" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  subnet_id = "subnet-9eb33ea1"
+  vpc_security_group_ids = ["sg-24264f51"]
 }
