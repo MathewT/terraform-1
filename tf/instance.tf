50c04a042dd8a0e4b564deb32e786d9e79b18232
Mathew Thomas <mathew.thomas@longdivision.com>
2018-02-26 22:02:34 -0500

    spot instances!
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- tf/instance.tf
+++ tf/instance.tf
@@ -1,10 +1,13 @@
-resource "aws_instance" "bc-redis-1" {
+resource "aws_spot_instance_request" "bc-redis-1" {
   ami           = "ami-ee4eac93"
   instance_type = "m4.large"
   subnet_id = "subnet-9eb33ea1"
   vpc_security_group_ids = ["sg-24264f51"]
   iam_instance_profile = "bc-machine-role-2"
   key_name = "vpc-47"
+
+  spot_price    = "0.05"
+  wait_for_fulfillment = true
 
   tags {
     Name = "bc-redis-1"
