5e5af8cffb128c742084737b02672991f81c62c0
Mathew Thomas <mathew.thomas@longdivision.com>
2018-02-26 21:52:46 -0500

    Ubuntu 16 ami
    Signed-off-by: Mathew Thomas <mathew.thomas@longdivision.com>

--- tf/instance.tf
+++ tf/instance.tf
@@ -1,6 +1,18 @@
 resource "aws_instance" "bc-redis-1" {
-  ami           = "ami-5cd4a126"
-  instance_type = "t2.micro"
+  ami           = "ami-ee4eac93"
+  instance_type = "m4.large"
   subnet_id = "subnet-9eb33ea1"
   vpc_security_group_ids = ["sg-24264f51"]
+  iam_instance_profile = "bc-machine-role-2"
+  key_name = "vpc-47"
+
+  tags {
+    Name = "bc-redis-1"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get -y update
+              sudo apt-get -y install apache2
+              EOF
 }
